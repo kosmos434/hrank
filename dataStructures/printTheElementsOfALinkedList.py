84fae03d82c48a3bdc9b036cf5fe84cf83f4a38f
kosmos434 <kosmos434@protonmail.com>
2021-09-30 17:05:42 -0500

    add printLinkedList

---
+++ dataStructures/printTheElementsOfALinkedList.py
@@ -0,0 +1,16 @@
+# 🔗
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    while head is not None:
+        print(head.data)
+        head = head.next
